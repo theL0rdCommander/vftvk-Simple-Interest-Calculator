30580cb247da62c9e68125bd882dcb7719194993
Felipe García <37160341+theL0rdCommander@users.noreply.github.com>
2022-01-13 22:20:21 -0300

    v1.0

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
     
+    let interest = parseInt(principal) * parseInt(rate) *parseInt(years)/ 100;
+    let amount = parseInt( principal) + parseInt(interest);
+    let year = new Date().getFullYear() + parseInt(years);
+    
+    if (parseInt(principal) <= 0){
+        alert("Enter a positive number");
+    }else{
+        document.getElementById("result").innerHTML = 
+            `If you deposit <mark> ${principal}</mark>,<br>
+            at an interest rate of  <mark> ${rate}%</mark><br>
+            You will receive an amount of  <mark> ${amount}</mark>,<br>
+            in the year  <mark> ${year}</mark><br>`;
+    }
+    document.getElementById("principal").focus();
 }
-        
+
+function updateRate()
+{
+    document.getElementById("rateValue").innerText=document.getElementById("rate").value + "%";
+   // compute();
+}
+
+function fillTheBlanks(){
+    document.getElementById("principal").innerText = "100";
+}
